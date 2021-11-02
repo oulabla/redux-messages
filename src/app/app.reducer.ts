4d64ad6190311fc097be595db4f770552641a144
pushkov <pushkov@altec.ru>
2021-11-02 20:16:07 +0300

    dsfs

---
+++ src/app/app.reducer.ts
@@ -0,0 +1,15 @@
+import { combineReducers, Reducer } from "redux";
+import { ThreadsReducer, ThreadsState } from "./thread/threads.reducer";
+import { UsersReducer, UsersState } from "./user/users.reducer";
+
+export interface AppState{
+    users: UsersState;
+    threads: ThreadsState;
+}
+
+const rootReducer: Reducer<AppState> = combineReducers<AppState>({
+    users: UsersReducer,
+    threads: ThreadsReducer,
+})
+
+export default rootReducer;
